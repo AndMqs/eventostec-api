634bf83a4952aea76d049b0a51a4ba9216d31c8a
AndMqs <andresa.b.marques@hotmail.com>
2025-10-22 19:10:53 -0300

    Primeiro commit - projeto limpo sem credenciais

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,32 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @OneToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+
+
+
+}
